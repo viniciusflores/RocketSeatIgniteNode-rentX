29b69d1cfc761066228f9f67f8e61be65a87617d
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-08 18:01:05 -0300

    :construction: add categories model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,22 @@
 import { Request, Response, Router } from 'express';
 import { v4 as uuidV4 } from 'uuid';
 
+import { Category } from '../model/Category';
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = { id: uuidV4(), name, description };
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
