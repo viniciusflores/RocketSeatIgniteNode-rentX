7ef7f170669aacbd12af5ebb8d36a79a7fba685f
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-08 21:04:32 -0300

    :construction: include listCategories

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from 'express';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
 import { CreateSpecificationService } from '../modules/cars/service/CreateSpecificationService';
 
 const specificationsRoutes = Router();
