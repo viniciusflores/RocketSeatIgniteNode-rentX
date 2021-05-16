061893536572305b08284f3dd110d5896e8e72c8
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-15 22:25:59 -0300

    :construction: Create user session

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,54 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+    if (!passwordMatched) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const token = sign({}, '7f5965a85b12e47842fb66dd1d7658e2', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
