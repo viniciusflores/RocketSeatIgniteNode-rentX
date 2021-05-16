8a8be6ea30e0bd51b08174bd7b295ba659dfac4a
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-16 00:10:39 -0300

    :sparkles: Complete upload user avatar

--- src/middlewares/ensureAuthenticate.ts
+++ src/middlewares/ensureAuthenticate.ts
@@ -33,6 +33,10 @@ export async function ensureAuthenticate(
       throw new AppError('Users not exists', 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch {
     throw new AppError('Invalid token', 401);
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import { ensureAuthenticate } from '../middlewares/ensureAuthenticate';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 
+const uploadAvatarConfig = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticate,
+  uploadAvatarConfig.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 export { usersRoutes };
