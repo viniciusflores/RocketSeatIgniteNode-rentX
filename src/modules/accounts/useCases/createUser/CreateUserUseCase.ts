9691b575cb5953c9d5676927c8065385b7a73f3a
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-15 21:49:18 -0300

    :sparkles: include crypted passwd and validate unique email

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
@@ -10,10 +11,23 @@ class CreateUserUseCase {
     private usersRepository: IUsersRepository,
   ) {}
 
-  async execute({ name, password, email, driver_license }: ICreateUserDTO) {
+  async execute({
+    name,
+    password,
+    email,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHashed = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHashed,
       email,
       driver_license,
     });
