22766eb07b0db3eec75102921475ef0184bead8b
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-24 14:45:47 -0300

    :sparkles: Create user admin seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,14 +2,15 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
-import '@shared/infra/typeorm';
-import '@shared/container';
 
+import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
