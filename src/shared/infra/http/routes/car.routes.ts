dee1c6c776f33d04075261a1b2fecdbd6e650d8e
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-24 16:42:17 -0300

    :sparkles: Implement middlewares in routes

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCars/CreateCarController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticate } from '@shared/infra/http/middlewares/ensureAuthenticate';
 
 const carRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carRoutes.post('/', createCarController.handle);
+carRoutes.post(
+  '/',
+  ensureAuthenticate,
+  ensureAdmin,
+  createCarController.handle,
+);
 
 export { carRoutes };
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticate } from '@shared/infra/http/middlewares/ensureAuthenticate';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+  '/',
+  ensureAuthenticate,
+  ensureAdmin,
+  createSpecificationController.handle,
+);
 
 export { specificationsRoutes };
