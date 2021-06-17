d4414d4a62b247e33d863181a23989f2ff990151
Vinicius Flores <viniciusflores379@gmail.com>
2021-06-17 12:39:07 -0300

    :sparkles: Create Devolution Rental Service

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 import { ensureAuthenticate } from '@shared/infra/http/middlewares/ensureAuthenticate';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post('/', ensureAuthenticate, createRentalController.handle);
+rentalRoutes.post(
+  '/devolution/:id',
+  ensureAuthenticate,
+  devolutionRentalController.handle,
+);
 
 export { rentalRoutes };
