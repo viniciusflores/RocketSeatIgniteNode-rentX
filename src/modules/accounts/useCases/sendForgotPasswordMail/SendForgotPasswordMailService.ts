67a664ef1948c834bf3ee847e1effdffc09e65a6
Vinicius Flores <viniciusflores379@gmail.com>
2022-02-23 16:49:36 -0300

    :construction: Started reset password service

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailService.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailService.ts
@@ -1,3 +1,5 @@
+import { template } from 'handlebars';
+import { resolve } from 'path';
 import { inject, injectable } from 'tsyringe';
 import { v4 as uuidV4 } from 'uuid';
 
@@ -23,6 +25,15 @@ class SendForgotPasswordMailService {
   async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
+    const templatePath = resolve(
+      __dirname,
+      '..',
+      '..',
+      'views',
+      'emails',
+      'forgotPassword.hbs',
+    );
+
     if (!user) {
       throw new AppError('User does not exists!');
     }
@@ -37,10 +48,16 @@ class SendForgotPasswordMailService {
       expires_date,
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    };
+
     await this.mailProvider.sendMail(
       email,
       'Recuperação de senha',
-      `O link para reset é ${token}`,
+      variables,
+      templatePath,
     );
   }
 }
