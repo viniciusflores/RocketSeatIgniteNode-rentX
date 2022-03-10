b4c245c5f50fe61eb143821e631100fc3110ab97
Vinicius Flores <viniciusflores379@gmail.com>
2022-03-10 20:59:25 -0300

    :sparkles: Implements reset password

--- src/shared/infra/http/routes/passwordRecovery.routes.ts
+++ src/shared/infra/http/routes/passwordRecovery.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const passwordRecoveryRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRecoveryRoutes.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRecoveryRoutes.post('/reset', resetPasswordUserController.handle);
 
 export { passwordRecoveryRoutes };
